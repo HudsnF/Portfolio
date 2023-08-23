7d5a9b7e9ad609da53023eca58c0fc1ba144ab17
user.name <hudsonfariadev@gmail.com>
2023-08-23 11:48:19 -0300

    new project

--- src/app/projects/ProjectCard.js
+++ src/app/projects/ProjectCard.js
@@ -5,6 +5,20 @@ export default function ProjectCard() {
     return (
         <div className="w-3/4 flex flex-wrap justify-center text-slate-800">
             <div className="w-96 bg-neutral-200 m-2 p-4  flex flex-col items-center justify-start gap-4 rounded-tl-3xl rounded-br-3xl shadow-md shadow-slate-800">
+                <h1 className="font-bold text-slate-500 shadow-sm shadow-yellow-500 px-2 rounded-xl text-2xl">Amazon Clone</h1>
+                <div className="overflow-y-scroll h-44 scrollbar-thin scrollbar-track-orange-950 scrollbar-thumb-orange-500">
+                    <Image src={"/img/amazon-clone.jpeg"} width={500} height={100} alt="" />
+                </div>
+                <div className="flex gap-4">
+                    <a href="https://amazon-clone-umber-eta.vercel.app/" target="_blank" className="bg-slate-900 px-4 py-1 text-lg flex items-center rounded-xl gap-2 shadow-sm shadow-slate-800 border-2 border-black text-white font-semibold hover:bg-slate-700 transition-all duration-300">Site <Link size={16} /></a>
+                    <a href="https://github.com/HudsnF/Amazon_clone" target="_blank" className="bg-slate-900 px-3 py-1 text-lg flex items-center rounded-xl gap-2 shadow-sm shadow-slate-800 border-2 border-black text-white font-semibold hover:bg-slate-700 transition-all duration-300">Code<FileCode2 size={16} /></a>
+                </div>
+                <div className="p-2 text-lg text-center">
+                    <p>Uma copia do layout e paginas do site da Amazon, utilizado o link do <span className="font-semibold">Next.Js</span> para a navegação das páginas principais do site. Customizado com <span className="font-semibold">TailwindCSS</span>.</p>
+                </div>
+            </div>
+
+            <div className="w-96 bg-neutral-200 m-2 p-4  flex flex-col items-center justify-start gap-4 rounded-tl-3xl rounded-br-3xl shadow-md shadow-slate-800">
                 <h1 className="font-bold text-slate-500 shadow-sm shadow-yellow-500 px-2 rounded-xl text-2xl">Spotify Clone</h1>
                 <div>
                     <Image src={"/img/spotify.jpeg"} width={500} height={100} alt="" />
